aeb604a334073dba184e86bd860647b163f7c574
silsha <hallo@silsha.me>
2014-11-12 18:13:01 +0100

    Initial Commit

---
+++ Netzkatze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Netzkatze
+//
+//  Created by silsha on 12/11/14.
+//  Copyright (c) 2014 silsha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
