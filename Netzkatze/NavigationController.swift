ed3132778480781570fae581af0d436297e54067
silsha <hallo@silsha.me>
2014-11-12 23:58:12 +0100

    first version

--- Netzkatze/NavigationController.swift
+++ Netzkatze/NavigationController.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+
+class NavigationController: UINavigationController, UIViewControllerTransitioningDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Status bar white font
+        self.navigationBar.barStyle = UIBarStyle.Black
+        self.navigationBar.tintColor = UIColor.whiteColor()
+    }
+}
+
--- Netzkatze/ViewController.swift
+++ Netzkatze/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var naivationbar: UINavigationItem!
+    @IBOutlet weak var input: UITextField!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendTweet(sender: UIButton) {
+        let url : String = "https://netzkatze.ohai.su/~lutoma/cgi-bin/netzkatze-jsonsubmit.py";
+        var request : NSMutableURLRequest = NSMutableURLRequest();
+        request.URL = NSURL(string: url);
+        request.HTTPMethod = "POST";
+        request.HTTPBody = ("{\"tweet\":\"\(input.text)\"}" as NSString).dataUsingEncoding(NSUTF8StringEncoding);
+        
+        
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.input.text = "";
+            }
+            
+            
+        })
+        
+    }
 
 }
 
