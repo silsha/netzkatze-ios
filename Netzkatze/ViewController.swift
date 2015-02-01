61987b97c2fb66b8bf9cf693b6d85d86151315f3
silsha <hallo@silsha.me>
2015-02-01 07:09:47 +0100

    Some changes. :3 Meow.

--- Netzkatze/ViewController.swift
+++ Netzkatze/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
     
     
     @IBOutlet weak var naivationbar: UINavigationItem!
-    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var sending: UILabel!
+    
+    
 
     
     override func viewDidLoad() {
@@ -21,22 +23,64 @@ class ViewController: UIViewController {
         dispatch_async(dispatch_get_main_queue()) {
             self.sending.text = "";
         }
+        textView.text = "Maunz. Wie geht’s dir?"
+        textView.textColor = UIColor.lightGrayColor()
+        
+        textView.becomeFirstResponder()
+        textView!.delegate = self
+        
+        textView.selectedTextRange = textView.textRangeFromPosition(textView.beginningOfDocument, toPosition: textView.beginningOfDocument)
+    }
+    
+    func textView(textView: UITextView!, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        
+        // Combine the textView text and the replacement text to
+        // create the updated text string
+        let currentText:NSString = textView.text
+        let updatedText = currentText.stringByReplacingCharactersInRange(range, withString:text)
+        
+        // If updated text view will be empty, add the placeholder
+        // and set the cursor to the beginning of the text view
+        if countElements(updatedText) == 0 {
+            
+            textView.text = "Maunz. Wie geht’s dir?"
+            textView.textColor = UIColor.lightGrayColor()
+            
+            textView.selectedTextRange = textView.textRangeFromPosition(textView.beginningOfDocument, toPosition: textView.beginningOfDocument)
+            
+            return false
+        }
+            
+            // Else if the text view's placeholder is showing and the
+            // length of the replacement string is greater than 0, clear
+            // the text view and set its color to black to prepare for
+            // the user's entry
+        else if textView.textColor == UIColor.lightGrayColor() && countElements(text) > 0 {
+            textView.text = nil
+            textView.textColor = UIColor.blackColor()
+        }
+        
+        return true
+    }
+    
+    func textViewDidChangeSelection(textView: UITextView!) {
+        if self.view.window != nil {
+            if textView.textColor == UIColor.lightGrayColor() {
+                textView.selectedTextRange = textView.textRangeFromPosition(textView.beginningOfDocument, toPosition: textView.beginningOfDocument)
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     @IBAction func sendTweet(sender: UIButton) {
         dispatch_async(dispatch_get_main_queue()) {
-            self.sending.text = "Sending …";
+            self.sending.text = "Maaunz …";
         }
         let url : String = "https://netzkatze.ohai.su/~lutoma/cgi-bin/netzkatze-jsonsubmit.py";
         var request : NSMutableURLRequest = NSMutableURLRequest();
         request.URL = NSURL(string: url);
         request.HTTPMethod = "POST";
-        request.HTTPBody = ("{\"tweet\":\"\(input.text)\"}" as NSString).dataUsingEncoding(NSUTF8StringEncoding);
+        request.HTTPBody = ("{\"tweet\":\"\(textView.text)\"}" as NSString).dataUsingEncoding(NSUTF8StringEncoding);
         
         
         
@@ -45,7 +89,7 @@ class ViewController: UIViewController {
             let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
             
             dispatch_async(dispatch_get_main_queue()) {
-                self.input.text = "";
+                self.textView.text = "";
                 self.sending.text = "";
             }
             
