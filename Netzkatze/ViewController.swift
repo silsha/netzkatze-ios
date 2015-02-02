0b5e7e38c6ca27799454c1c1759d4ba0e7c8c065
silsha <hallo@silsha.me>
2015-02-02 17:56:41 +0100

    More changes. :> Nyau.

--- Netzkatze/ViewController.swift
+++ Netzkatze/ViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import TwitterKit
+import Fabric
 
-class ViewController: UIViewController, UITextViewDelegate {
+
+class ViewController: UIViewController, UITextViewDelegate, UITableViewDataSource, UITableViewDelegate, TWTRTweetViewDelegate {
     
     
     @IBOutlet weak var naivationbar: UINavigationItem!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var sending: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
+    // setup a 'container' for Tweets
+    var tweets: [TWTRTweet] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
-
+    var prototypeCell: TWTRTweetTableViewCell?
+    let tweetTableCellReuseIdentifier = "TweetCell"
+    var isLoadingTweets = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +39,30 @@ class ViewController: UIViewController, UITextViewDelegate {
         textView.textColor = UIColor.lightGrayColor()
         
         textView.becomeFirstResponder()
-        textView!.delegate = self
+        textView.delegate = self;
+        
+        tableView.dataSource = self;
+        tableView.delegate = self;
         
         textView.selectedTextRange = textView.textRangeFromPosition(textView.beginningOfDocument, toPosition: textView.beginningOfDocument)
+        
+        // Create a single prototype cell for height calculations.
+        self.prototypeCell = TWTRTweetTableViewCell(style: .Default, reuseIdentifier: tweetTableCellReuseIdentifier)
+        
+        // Register the identifier for TWTRTweetTableViewCell.
+        self.tableView.registerClass(TWTRTweetTableViewCell.self, forCellReuseIdentifier: tweetTableCellReuseIdentifier)
+        // Setup table data
+        
+        loadTweets()
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Make sure the navigation bar is not translucent when scrolling the table view.
+        self.navigationController?.navigationBar.translucent = false
+    }
+    
     
     func textView(textView: UITextView!, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
         
@@ -70,6 +102,129 @@ class ViewController: UIViewController, UITextViewDelegate {
             }
         }
     }
+    
+    class CustomObject {
+        var id: Int = 0
+        
+        init(json: Dictionary<String, AnyObject>) {
+            id = (json["id"] as NSNumber).integerValue
+        }
+    }
+    
+    func loadTweets() {
+        // Do not trigger another request if one is already in progress.
+        if self.isLoadingTweets {
+            return
+        }
+        self.isLoadingTweets = true
+        let statusesShowEndpoint = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+        let params = ["user_id": "899670319"]
+        var clientError : NSError?
+        
+        Twitter.sharedInstance().logInGuestWithCompletion {
+            (guestSession, error) -> Void in
+            if (guestSession != nil) {
+        
+        let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod(
+                "GET", URL: statusesShowEndpoint, parameters: params,
+                error: &clientError)
+                
+            
+        
+        if request != nil {
+
+                    Twitter.sharedInstance().APIClient.sendTwitterRequest(request) {
+                        (response, data, connectionError) -> Void in
+                        if (connectionError == nil) {
+                            var jsonError : NSError?
+                            var json      = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: nil) as Array<Dictionary<String, AnyObject>>
+                            var customObjects = json.map { dict in CustomObject(json: dict) }
+                            var tweetids : [String] = []
+                            for i in customObjects {
+                                tweetids.append(String(i.id));
+                            }
+                            Twitter.sharedInstance().APIClient
+                                .loadTweetsWithIDs(tweetids) {
+                                    (tweets, error) -> Void in
+                                    self.tweets = [];
+                                    for i in tweets {
+                                        self.tweets.append(i as TWTRTweet)
+                                    }
+                            }
+                        
+                        }
+                        else {
+                            println("Error: \(connectionError)")
+                        }
+                    
+            
+            }}
+    
+        else {
+            println("Error: \(clientError)")
+        }
+        
+            }}}
+    
+    
+
+
+//            (tweetIDs) {
+//                (twttrs, error) -> Void in
+//
+//                // If there are tweets do something magical
+//                if ((twttrs) != nil) {
+//
+//                    // Loop through tweets and do something
+//                    for i in twttrs {
+//                        // Append the Tweet to the Tweets to display in the table view.
+//                        self.tweets.append(i as TWTRTweet)
+//                    }
+//                } else {
+//                    println(error)
+//                }
+    
+    func refreshInvoked() {
+        // Trigger a load for the most recent Tweets.
+        loadTweets()
+    }
+    
+    // MARK: TWTRTweetViewDelegate
+    func tweetView(tweetView: TWTRTweetView!, didSelectTweet tweet: TWTRTweet!) {
+        // Display a Web View when selecting the Tweet.
+        let webViewController = UIViewController()
+        let webView = UIWebView(frame: webViewController.view.bounds)
+        webView.loadRequest(NSURLRequest(URL: tweet.permalink))
+        webViewController.view = webView
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of Tweets.
+        return tweets.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Retrieve the Tweet cell.
+        let cell = tableView.dequeueReusableCellWithIdentifier(tweetTableCellReuseIdentifier, forIndexPath: indexPath) as TWTRTweetTableViewCell
+        
+        // Assign the delegate to control events on Tweets.
+        cell.tweetView.delegate = self
+        
+        // Retrieve the Tweet model from loaded Tweets.
+        let tweet = tweets[indexPath.row]
+        
+        // Configure the cell with the Tweet.
+        cell.configureWithTweet(tweet)
+        
+        // Return the Tweet cell.
+        return cell
+    }
+    
+
+    
+    
 
 
     @IBAction func sendTweet(sender: UIButton) {
@@ -91,6 +246,12 @@ class ViewController: UIViewController, UITextViewDelegate {
             dispatch_async(dispatch_get_main_queue()) {
                 self.textView.text = "";
                 self.sending.text = "";
+                self.isLoadingTweets = false;
+                let delay = 1.5 * Double(NSEC_PER_SEC)
+                let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+                dispatch_after(time, dispatch_get_main_queue()) {
+                    self.refreshInvoked();
+                }
             }
             
             
